100d2d37e7374650b77528f99af2c423a405aa04
Your Angelo <angelo18420@gmail.com>
2024-05-02 17:24:19 +0800

    payroll

--- payroll/payroll_generate.php
+++ payroll/payroll_generate.php
@@ -18,6 +18,7 @@
 			<tr>
 				<td align="center">'.$row['employee_name'].'</td>
 				<td align="center">'.$row['employee_id'].'</td>
+				<td align="center">'.$row['employee_id'].'</td>
 				<td align="center">'.$row['paystatus'].'</td>
 				<td align="center">'.number_format($row['netpay'], 2).'</td>
 			</tr>
@@ -47,7 +48,7 @@
 	$to_title = date('M d, Y', strtotime($ex[1]));
 
 	require_once('../tcpdf/tcpdf.php');  
-    $pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);  
+    $pdf = new TCPDF('L', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);  
     $pdf->SetCreator(PDF_CREATOR);  
     $pdf->SetTitle('EZ PAYROLL SYSTEM');  
     $pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);  
@@ -59,20 +60,34 @@
     $pdf->setPrintHeader(false);  
     $pdf->setPrintFooter(false);  
     $pdf->SetAutoPageBreak(TRUE, 10);  
-    $pdf->SetFont('helvetica', '', 11);  
+    $pdf->SetFont('helvetica', '', 7);  
     $pdf->AddPage();  
     $content = '';  
     $content .= '
-      	<h2 align="center">EZ PAYROLL SYSTEM</h2>
-      	<h4 align="center">'.$from_title." - ".$to_title.'</h4>
-      	<table border="1" cellspacing="0" cellpadding="3">  
-           <tr>  
-           		<th width="30%" align="center"><b>Employee Name</b></th>
-                <th width="30%" align="center"><b>Employee ID</b></th>
-				<th width="20%" align="center"><b>Status</b></th> 
-				<th width="20%" align="center"><b>Net Pay</b></th> 
-				
-           </tr>  
+	<h2 align="center">EZ PAYROLL SYSTEM</h2>
+	<h4 align="center">'.$from_title." - ".$to_title.'</h4>
+	<table border="1" cellspacing="0" cellpadding="3">  
+	<tr>  
+		<th width="3%"  align="center"><b>No</b></th>
+		<th width="8%"  align="center"><b>Name</b></th>
+		<th width="5%"  align="center"><b>Position</b></th>
+		<th width="6%"  align="center"><b>Employee ID</b></th>
+		<th width="6%"  align="center"><b>Monthly Salary</b></th>
+		<th width="6%"  align="center" colspan="2"><b>Other Compensation</b></th>
+		<th width="6%" align="center" ><b>Gross Amount </b></th>
+		<th width="6%" colspan="6" align="center" ><b>Deductions</b></th>
+		<th width="6%" align="center" ><b>TOTAL DEDUCTIONS</b></th>
+		<th width="6%"  align="center" ><b>NET AMOUNT DUE<br>* 1st half<br>. 2nd half</b></th>
+		
+	</tr>
+	<tr>
+			<td colspan="4"></td> <!-- Empty cells for Employee Name, Position, Employee ID, and Monthly Salary -->
+			<th width="6%" align="center"><b>Personal Economic Relief Allowance</b></th>
+			<th width="6%" align="center"><b>Additional Compens.</b></th>
+			<td></td> <!-- Empty cell for Status -->
+			<td></td> <!-- Empty cell for Net Pay -->
+	</tr>
+
       ';  
     $content .= generateRow($from, $to, $conn);  
     $content .= '</table>';  
