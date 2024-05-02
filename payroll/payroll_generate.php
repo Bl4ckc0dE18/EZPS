6bd2b6044038405d5941ebb725543190e38c4c20
Bl4ckc0dE18 <angelo18420@gmail.com>
2024-05-03 04:35:13 +0800

    PAYROLL

--- payroll/payroll_generate.php
+++ payroll/payroll_generate.php
@@ -3,7 +3,7 @@
 
 	function generateRow($from, $to, $conn){
 		$contents = '';
-	 	
+		$totalSalary = 0; 
 		//$sql = "SELECT * FROM payslip WHERE datefrom >= '$from' AND dateto <= '$to'";
 
 		$sql = "SELECT * FROM payslip WHERE datefrom >= '$from' AND dateto <= '$to'";
@@ -13,45 +13,58 @@
 		//$total = 0;
 		//$total1 = 0;
 		while($row = $query->fetch_assoc()){
-			$total = $row['netpay'];
 			
+			$netPay = $row['netpay'];
+			$totalSalary += $netPay; 
 			/**/$contents .= '
+			<style>
+				.right-border {
+				border-right: 1px solid black; 
+				}
+				.bottom-border {	
+				border-bottom: 1px solid black;		
+				}
+			</style>
 			<tr>
-				<td  align="center">'.$no++.'</td>
-				<td  align="center">'.$row['employee_name'].'</td>
+				<td border="1" align="center">'.$no++.'</td>
+				<td border="1" align="center">'.$row['employee_name'].'</td>
 				
-				<td  align="center"></td>
+				<td border="1" align="center"></td>
 
-				<td  align="center">'.$row['employee_id'].'</td>
+				<td border="1" align="center">'.$row['employee_id'].'</td>
 				
-				<td  align="right">'.number_format($row['netpay'], 2).'</td>
-				<td  align="right">PERA</td>
+				<td border="1" align="right">'.number_format($row['netpay'], 2).'</td>
+				<td border="1" align="right">PERA</td>
 				
-				<td align="right">AC</td>
-				<td width="7%" align="right">GAE</td>
-
-				
-				<td align="center"><b>@<br>#<br>.<br>&<br>!</b></td>
-				<td align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
-
-				<td align="center"><b>a<br>b<br>c<br>d<br>e</b></td>
-				<td align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
-
-				<td align="center"><b>f<br>g<br>h<br>i<br>j</b></td>
-				<td align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
-
-				<td align="center"><b>k<br>l<br>m<br>n<br>o</b></td>
-				<td align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
-
-				<td align="center"><b>p<br>q<br>r<br>s<br>t</b></td>
-				<td align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
-
-				<td align="center"><b>u<br>v<br>w<br>x<br>y</b></td>
-				<td align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
+				<td border="1" align="right">AC</td>
+				<td border="1" width="7%" align="right">GAE</td>
 
 				
-				<td  width="7%" align="right">TD </td>
-				<td width="7%"align="right">NAE  </td>
+				<td class="bottom-border" align="center"><b>@<br>#<br>.<br>&<br>!</b></td>
+				<td class="bottom-border right-border"  align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
+
+				<td class="bottom-border" align="center"><b>a<br>b<br>c<br>d<br>e</b></td>
+				<td class="bottom-border right-border" align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
+
+				<td class="bottom-border" align="center"><b>f<br>g<br>h<br>i<br>j</b></td>
+				<td class="bottom-border right-border" align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
+
+				<td class="bottom-border" align="center"><b>k<br>l<br>m<br>n<br>o</b></td>
+				<td class="bottom-border right-border" align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
+
+				<td class="bottom-border" align="center"><b>p<br>q<br>r<br>s<br>t</b></td>
+				<td class="bottom-border right-border" align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
+
+				<td class="bottom-border" align="center"><b>u<br>v<br>w<br>x<br>y</b></td>
+				<td class="bottom-border right-border" align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
+
+				
+				<td border="1" width="7%" align="right">TD </td>
+				
+
+				<td class="bottom-border" width="2%" align="center"><b>*<br><br>.<br></b></td>
+				<td class="bottom-border right-border" width="5%" align="right"><b>1st half<br><br>2nd half</b></td>
+
 
 			</tr>
 			';
@@ -60,12 +73,112 @@
 	 
 		
 
-		/*$contents .= '
+		/**/$contents .= '
+		<style>
+    		.right-border {
+        	border-right: 1px solid black; 
+    		}
+			.bottom-border {	
+			border-bottom: 1px solid black;		
+			}
+		</style>
+
 			<tr>
-				<td colspan="3" align="right"><b>Total</b></td>
-				<td align="center"><b>'.number_format($total, 2).'</b></td>
+				<td border="1"  align="center"></td>
+				<td border="1"  align="center">SHEET TOTAL</td>
+				
+				<td border="1" align="center">-</td>
+
+				<td border="1"  align="center"></td>
+				
+				<td border="1" align="right">'.number_format($totalSalary, 2).'</td>
+				<td border="1" align="right">PERA</td>
+				
+				<td border="1" align="right">AC</td>
+				<td border="1" width="7%" align="right">GAE</td>
+
+				
+				<td class="bottom-border" align="center"><b>@<br>#<br>.<br>&<br>!</b></td>
+				<td class="bottom-border right-border" align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
+
+				<td class="bottom-border" align="center"><b>a<br>b<br>c<br>d<br>e</b></td>
+				<td class="bottom-border right-border" align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
+
+				<td class="bottom-border" align="center"><b>f<br>g<br>h<br>i<br>j</b></td>
+				<td class="bottom-border right-border" align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
+
+				<td class="bottom-border" align="center"><b>k<br>l<br>m<br>n<br>o</b></td>
+				<td class="bottom-border right-border" align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
+
+				<td class="bottom-border" align="center"><b>p<br>q<br>r<br>s<br>t</b></td>
+				<td class="bottom-border right-border" align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
+
+				<td class="bottom-border" align="center"><b>u<br>v<br>w<br>x<br>y</b></td>
+				<td class="bottom-border right-border" align="right"><b>-<br>-<br>-<br>-<br>-</b></td>
+
+				
+				<td border="1" width="7%" align="right">TD </td>
+				
+
+				<td class="bottom-border" width="2%" align="center"><b>*<br><br>.<br></b></td>
+				<td class="bottom-border right-border" width="5%"align="right"><b>1st half<br><br>2nd half</b></td>
+
 			</tr>
-		';*/
+		';
+
+//footer
+		$contents .= '
+		<style>
+			.right-border {
+        	border-right: 1px solid black; 
+    		}
+			.bottom-border {	
+			border-bottom: 1px solid black;		
+			}
+			.left-border {	
+				border-left: 1px solid black;		
+			}
+			.top-border {	
+					border-top: 1px solid black;		
+			}
+		</style>
+			<tr>
+				<td border="1" rowspan="1"  align="center">A</td>
+				<td colspan="9" align="left">CERTIFIED: Service duly rendered as stated</td>
+
+				<td border="1"colspan="1" align="center">C</td>
+				<td class="right-border" colspan="12" align="left">  APPROVED FOR PAYMENT:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___________________________ P___________________</td>
+				
+			</tr>
+
+			<tr>
+				<td class="left-border right-border" colspan="10" align="center" ><br></td>
+				<td class="right-border" colspan="13" align="center" ><br></td>
+			</tr>
+			<tr>
+				
+				<td class="left-border bottom-border" colspan="2" align="center" ><br></td>
+				<td class="bottom-border right-border "colspan="8" align="left"><b>ATTY. CHRISTOPHER M. MORTEL</b>
+				<br>Supervising Adminstrative Officer, HRMS</td>
+
+				<td class="left-border bottom-border" colspan="3" align="center" ><br></td>
+				<td class="bottom-border right-border "colspan="10" align="left"><b>PURABELLA R. ARGON</b>
+				<br>Vice President for Adminstrative and Finance</td>
+			</tr>
+
+			<tr>
+				<td border="1" rowspan="1"  align="center">D</td>
+				<td colspan="9" align="left">CERTIFIED: Service duly rendered as stated</td>
+				<td border="1" rowspan="1"  align="center">D</td>
+				<td colspan="10" align="left">CERTIFIED: Service duly rendered as stated</td>
+			</tr>
+
+			
+		';
 		return $contents;
 	}
 		
@@ -97,45 +210,64 @@
     $pdf->AddPage();  
     $content = '';  
     $content .= '
+
+	<style>
+    		.right-border {
+        	border-right: 1px solid black;
+			
+    		}
+
+			.bottom-border {
+				
+				border-bottom: 1px solid black;
+				}
+			
+	</style>
+
 	<h2 align="center">EZ PAYROLL SYSTEM</h2>
 	<h4 align="center">'.$from_title." - ".$to_title.'</h4>
-	<table border="1" cellspacing="0" cellpadding="3">  
-	<tr>  
-		<th width="3%" rowspan="2" align="center"><b><br><br><br><br>NO</b></th>
-		<th width="8%" rowspan="2" align="center"><b><br><br><br><br>NAME</b></th>
-		<th width="6%" rowspan="2" align="center"><b><br><br><br><br>POSITION</b></th>
-		<th width="4%" rowspan="2" align="center"><b><br><br><br><br>EMPLO<br>YEE <br>NO</b></th>
-		<th width="6%" rowspan="2" align="center"><b><br><br><br><br>MONTHLY SALARY</b></th>
-		<th width="12%" height="2" rowspan="1"  align="center"><b><br>OTHER COMPENSATION</b></th>
+	<table border="0" cellspacing="0" cellpadding="3">  
 
-		<th width="7%" rowspan="2"	align="center" ><b><br><br><br><br>GROSS AMOUNT <BR>EARNED</b></th>
-		<th width="42%" rowspan="1" colspan="7" align="center" ><b><br><br>DEDUCTIONS</b></th>
+	<tr>  
+		<th border="1" width="3%" rowspan="2" align="center"><b><br><br><br><br>NO</b></th>
+		<th border="1" width="8%" rowspan="2" align="center"><b><br><br><br><br>NAME</b></th>
+		<th border="1" width="6%" rowspan="2" align="center"><b><br><br><br><br>POSITION</b></th>
+		<th border="1" width="4%" rowspan="2" align="center"><b><br><br><br><br>EMPLO<br>YEE <br>NO</b></th>
+		<th border="1"  width="6%" rowspan="2" align="center"><b><br><br><br><br>MONTHLY SALARY</b></th>
+		<th border="1" width="12%" height="2" rowspan="1"  align="center"><b><br>OTHER COMPENSATION</b></th>
+
+		<th border="1" width="7%" rowspan="2"	align="center" ><b><br><br><br><br><br>GROSS AMOUNT <BR>EARNED</b></th>
+		<th border="1" width="42%" rowspan="1" colspan="7" align="center" ><b><br><br>DEDUCTIONS</b></th>
 		
-		<th width="7%" rowspan="2" align="center" ><b><br><br><br><br>TOTAL<br>DEDUCTIONS</b></th>
-		<th width="7%" rowspan="2" align="center" ><b><br><br><br><br>NET AMOUNT DUE<br> 1st half<br>. 2nd half</b></th>
+		<th border="1" width="7%" rowspan="2" align="center" ><b><br><br><br><br>TOTAL<br>DEDUCTIONS</b></th>
+		<th border="1" width="7%" rowspan="1" align="center" ><b><br><br><br><br>NET AMOUNT DUE<br> <br>. </b></th>
 	</tr>
+
 	<tr>
 			
-			<th width="6%" align="center"><b><br>PERSONAL<br>ECONOMIC<br>RELIEF<br>ALLOWANCE</b></th>
+			<th border="1" width="6%" align="center"><b><br>PERSONAL<br>ECONOMIC<br>RELIEF<br>ALLOWANCE</b></th>
 			<th width="6%" align="center"><b><br><br>ADDITIONAL COMPENS.</b></th>
 
-			<th width="2%" align="center"><b>@<br>#<br>.<br>&<br>!</b></th>
-			<th width="5%"><b>Disallowance<br>Ref-Sal<br>Ref-Ocom<br>NHMC<br>MP2</b></th>
+			<th class="bottom-border" width="2%" align="center"><b>@<br>#<br>.<br>&<br>!</b></th>
+			<th class="bottom-border right-border" width="5%"><b>Disallowance<br>Ref-Sal<br>Ref-Ocom<br>NHMC<br>MP2</b></th>
 
-			<th width="2%" align="center"><b>a<br>b<br>c<br>d<br>e</b></th>
-			<th width="5%"><b>Integ-Ins<br>W/tax<br>Philhealth<br>GSIS MPL<br>GSIS Sal</b></th>
+			<th class="bottom-border" width="2%" align="center"><b>a<br>b<br>c<br>d<br>e</b></th>
+			<th class="bottom-border right-border" width="5%"><b>Integ-Ins<br>W/tax<br>Philhealth<br>GSIS MPL<br>GSIS Sal</b></th>
 
-			<th width="2%" align="center"><b>f<br>g<br>h<br>i<br>j</b></th>
-			<th width="5%"><b>GSIS Pol<br>GSIS ELA<br>GSIS Opin<br>GSIS OpLo<br>GFAL</b></th>
+			<th class="bottom-border" width="2%" align="center"><b>f<br>g<br>h<br>i<br>j</b></th>
+			<th class="bottom-border right-border" width="5%"><b>GSIS Pol<br>GSIS ELA<br>GSIS Opin<br>GSIS OpLo<br>GFAL</b></th>
 
-        	<th width="2%" align="center"><b>k<br>l<br>m<br>n<br>o</b></th>
-			<th width="5%"><b>GSIS HIP<br>GSIS CPL<br>GSIS SOS<br>GSIS Eplan<br>GSIS Ecard</b></th>
+        	<th class="bottom-border" width="2%" align="center"><b>k<br>l<br>m<br>n<br>o</b></th>
+			<th class="bottom-border right-border" width="5%"><b>GSIS HIP<br>GSIS CPL<br>GSIS SOS<br>GSIS Eplan<br>GSIS Ecard</b></th>
 
-        	<th width="2%" align="center"><b>p<br>q<br>r<br>s<br>t</b></th>
-			<th width="5%"><b>HDMF MPL<br>H\'DMF Res<br>HDMF Con<br>LBP<br>TUPM-Cd</b></th>
+        	<th  class="bottom-border" width="2%" align="center"><b>p<br>q<br>r<br>s<br>t</b></th>
+			<th class="bottom-border right-border" width="5%"><b>HDMF MPL<br>H\'DMF Res<br>HDMF Con<br>LBP<br>TUPM-Cd</b></th>
 
-        	<th width="2%" align="center"><b>u<br>v<br>w<br>x<br>y</b></th>
-			<th width="5%"><b>Fin Ass<br>GSIS Educ<br>TUPAEA<br>TUPFA<br>HDMF Eme</b></th>
+        	<th  class="bottom-border" width="2%" align="center"><b>u<br>v<br>w<br>x<br>y</b></th>
+			<th class="bottom-border right-border" width="5%"><b>Fin Ass<br>GSIS Educ<br>TUPAEA<br>TUPFA<br>HDMF Eme</b></th>
+
+			<th  class="bottom-border" width="2%" align="center"><b>*<br><br>.<br></b></th>
+			<th class="bottom-border right-border" width="5%" align="right" ><b>1st half<br><br>2nd half</b></th>
 
 			
 	</tr>
